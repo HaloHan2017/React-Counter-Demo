d91289246bbedabc375c8a04cdce400462f177b3
HaloHan2017 <1006653740@qq.com>
2020-08-08 14:23:08 +0800

    [React-Counter-Demo] [Jerry] refactor : rename CounterContainers to CounterGroupContainer

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import './App.css';
-import CounterApp from "./containers/CounterContainers";
+import CounterGroupContainer from "./containers/CounterGroupContainer";
 
 function App() {
     return (
         <div className="App">
             <header className="App-header">
-                <CounterApp/>
+                <CounterGroupContainer/>
             </header>
         </div>
     );
